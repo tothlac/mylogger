aacebf40c58216a2983abe59a0b2002c0266b002
Laszlo Toth <LaszloToth7@otpbank.hu>
2019-05-09 12:26:10 +0200

    fix

--- src/log_proba_logger.erl
+++ src/log_proba_logger.erl
@@ -7,6 +7,6 @@
 %log({'ReceivedMessage', {From, Message}})  ->
 %  io:format("Received message from: ~s, message: ~B", [From, Message]).
 -spec log(Msg) -> ok when
-    Msg :: #{type => 'ReceivedMessage', from => binary(), message => float(2.0, 3.0)}.
+    Msg :: #{type => 'ReceivedMessage', from => binary(), count => 1..10}.
 log(#{type := 'ReceivedMessage', from := From, message := Message})  ->
   io:format("Received message from: ~s, message: ~B", [From, Message]).
